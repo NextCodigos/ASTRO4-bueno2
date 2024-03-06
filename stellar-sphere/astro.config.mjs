9787cd0041159416c21291ea188e37c573e07769
NextCodigos <nebulanemesis372@gmail.com>
2024-03-06 19:33:29 +0100

    se renderiza en el momento del compilado export const prerender = true output:'hybrid'

--- stellar-sphere/astro.config.mjs
+++ stellar-sphere/astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output:'hybrid',
+  // output:'server', //no es tan rapido pero siempre tenemos el ultimo contenido disponible
+  // output:'static', //el mas rapido pero no renderiza informacion base de datos
   integrations: [tailwind()]
 });
